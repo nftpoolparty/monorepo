934e7d3d5fe11abff700bbb03c143be7dbb6da0c
Lawrence Forman <me@merklejerk.com>
2023-09-24 00:29:54 -0400

    --no-edit

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.8.0;
 import "forge-std/Script.sol";
 
 import {UniNftRouter} from "contracts/UniNftRouter.sol";
+import {PoolManager} from "v4-core/PoolManager.sol";
 
 contract Deploy is Script {
     function setUp() public {}
@@ -12,7 +13,6 @@ contract Deploy is Script {
         uint256 deployer = vm.envUint("DEPLOYER_PRIVATE_KEY");
 
         vm.startBroadcast(deployer);
-
-        // new NftPoolFactory();
+        new UniNftRouter(new PoolManager(50_000));
     }
 }
